0f7a97f0769b9f16b349a793b77450468123ea55
Dksmh <mhyu95@naver.com>
2022-08-26 11:53:31 +0900

    board crud

--- src/main/java/com/board/domain/post/PostResponse.java
+++ src/main/java/com/board/domain/post/PostResponse.java
@@ -16,34 +16,5 @@ public class PostResponse {
     private Boolean deleteYn;              // 삭제 여부
     private LocalDateTime insertTime;     // 등록일
     private LocalDateTime updateTime;    // 수정일
-    /*
-	public Long getId() {
-		return id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public int getCnt() {
-		return cnt;
-	}
-	public Boolean getNoticeYn() {
-		return noticeYn;
-	}
-	public Boolean getDeleteYn() {
-		return deleteYn;
-	}
-	public LocalDateTime getInsertTime() {
-		return insertTime;
-	}
-	public LocalDateTime getUpdateTime() {
-		return updateTime;
-	}*/  
-    
-
+  
 }
